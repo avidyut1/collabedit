97104ee6cca5a4f17bf9039062c184b160c05dcf
Ashesh Vidyut <avidyut1@gmail.com>
2017-01-04 09:28:06 +0530

    minor bug fix

--- colabedit/app/assets/javascripts/controllers/padCtrl.js
+++ colabedit/app/assets/javascripts/controllers/padCtrl.js
@@ -4,12 +4,15 @@
 var app = angular.module('colabedit');
 app.controller('PadCtrl', ['$scope', '$http', '$state', 'socket', function ($scope, $http, $state, socket){
     socket.emit('hash_id', {'hash_id': $state.params.hash_id});
-    $scope.dataChanged = function (e){
-    	if(e) {
-        	socket.emit('data', {'data': $scope.data, 'hash_id': $state.params.hash_id});
-    	}
-    };
+    var msg_server = null;
     socket.on('newdata', function (msg) {
         $scope.data = msg.data;
+        msg_server = msg.data;
+    });
+    $scope.$watch('data', function (newvalue, oldvalue){
+        if (newvalue === msg_server) {
+            return;
+        }
+        socket.emit('data', {'data': $scope.data, 'hash_id': $state.params.hash_id});
     });
 }]);
